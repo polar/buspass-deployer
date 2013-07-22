d4b36ed10059b450d69bbd7c5fe2dfae47b5f502
Polar Humenn <polar@adiron.com>
2013-07-22 18:00:08 -0400

    Initial Commit

--- app/models/deploy_swift_endpoint_job.rb
+++ app/models/deploy_swift_endpoint_job.rb
@@ -285,6 +285,7 @@ class DeploySwiftEndpointJob
           result = HerokuHeadless.heroku.put_config_vars(app_name, vars)
           if result && result.data[:body]
             vars_set = result.data[:body].keys
+            log "#{head}: Configuration Result #{result.inspect}"
             log "#{head}: Remote Configuration Variables #{vars_set.join(", ")} have been set for swift endpoint #{app_name}."
             set_status("Success:Configure")
           else
