aea63d95d0ffa3c7c77d85bdf1348291aef025ab
Polar Humenn <polar@adiron.com>
2013-07-24 14:47:02 -0400

    Initial Commit

--- script/perform.sh
+++ script/perform.sh
@@ -1,9 +1,7 @@
-bundle exec script/delayed_job -i 1 stop
-bundle exec script/delayed_job -i 2 stop
-bundle exec script/delayed_job -i 3 stop
-bundle exec script/delayed_job -i 4 stop
-sleep 10
+
 bundle exec script/delayed_job --queues=deploy-web -i 1 start
 bundle exec script/delayed_job --queues=deploy-web -i 2 start
 bundle exec script/delayed_job --queues=deploy-web -i 3 start
-bundle exec script/delayed_job --queues=deploy-web -i 4 run
+bundle exec script/delayed_job --queues=deploy-web -i 4 start
+bundle exec script/delayed_job --queues=deploy-web -i 5 start
+bundle exec script/delayed_job --queues=deploy-web -i 6 run
