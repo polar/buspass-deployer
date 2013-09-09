7f9a6bc2b3c815bd7d09f2ab61a17a37b044b7ca
Polar Humenn <polar@adiron.com>
2013-09-09 17:49:39 -0400

    Unix Endpoint Types

--- app/models/deploy_swift_endpoint_job.rb
+++ app/models/deploy_swift_endpoint_job.rb
@@ -359,7 +359,7 @@ class DeploySwiftEndpointJob
         end
       when "Unix"
         log "#{head}: Starting remote swift endpoint #{user_name}@#{app_name}."
-        result = Rush.bash unix_ssh_cmd("bash -- login -c cd buspass-web\\; bundle exec script/instance -e production &")
+        result = Rush.bash unix_ssh_cmd("bash -- login -c cd buspass-web\\; bundle exec script/instance -e production \\&")
         swift_endpoint.reload
         if result
           set_status("Success:Start")
@@ -409,7 +409,7 @@ class DeploySwiftEndpointJob
         end
       when "Unix"
         log "#{head}: Stopping remote swift endpoint #{user_name}@#{app_name}."
-        result = Rush.bash unix_ssh_cmd("bash --login -c cd buspass-web\\; bundle exec script/stop_instances")
+        result = Rush.bash unix_ssh_cmd('bash --login -c "cd buspass-web; bundle exec script/stop_instances"')
         if result
           swift_endpoint.reload
           set_status("Success:Stop")
@@ -458,8 +458,8 @@ class DeploySwiftEndpointJob
       when "Unix"
         begin
           log "#{head}: Restarting remote swift endpoint #{user_name}@#{app_name}."
-          result = Rush.bash unix_ssh_cmd("bash --login -c cd buspass-web\\; bundle exec script/stop_instances")
-          result = Rush.bash unix_ssh_cmd("bash --login -c cd buspass-web\\; bundle exec script/instance -e production \\&")
+          result = Rush.bash unix_ssh_cmd('bash --login -c "cd buspass-web; bundle exec script/stop_instances"')
+          result = Rush.bash unix_ssh_cmd('bash --login -c "cd buspass-web; bundle exec script/instance -e production &"')
           swift_endpoint.reload
           if result && result
             set_status("Success:Restart")
@@ -620,10 +620,10 @@ class DeploySwiftEndpointJob
       when "Unix"
         begin
           log "#{head}: Deploying swift endpoint #{user_name}@#{app_name}"
-          result = Rush.bash unix_ssh_cmd("test -e buspass-web && test -e buspass-web/script/stop_instances && bash --login -c cd buspass-web\\; bundle exec script/stop_instances")
+          result = Rush.bash unix_ssh_cmd('test -e buspass-web && test -e buspass-web/script/stop_instances && bash --login -c "cd buspass-web; bundle exec script/stop_instances"')
           result = Rush.bash unix_ssh_cmd("test -e buspass-web || git clone http://github.com/polar/buspass-web.git -b #{swift_endpoint.git_refspec}")
           result = Rush.bash unix_ssh_cmd("cd buspass-web; git pull; git submodule init; git submodule update")
-          result = Rush.bash unix_ssh_cmd("bash --login -c cd buspass-web\\; bundle install")
+          result = Rush.bash unix_ssh_cmd('bash --login -c "cd buspass-web; bundle install"')
           swift_endpoint.reload
           if result
             log "#{head}: Created swift endpoint #{app_name} - #{result.inspect}"
