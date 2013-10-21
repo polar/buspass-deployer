73c771f07c7285c011286c2933e75c843e16d727
Polar Humenn <polar@adiron.com>
2013-10-21 14:16:07 -0400

    Unix Endpoint Types

---
+++ app/models/deploy_restart_backend_jobspec.rb
@@ -0,0 +1,24 @@
+class DeployRestartBackendJobspec < Struct.new(:queue, :backend_id, :period)
+
+  def enqueue(delayed_job)
+    backend = Backends.find(backend_id)
+    if backend
+      puts "Backend #{backend.name} will be restarted every #{period} seconds."
+    else
+      puts "Backend not found"
+    end
+  end
+
+  def perform
+    MongoMapper::Plugins::IdentityMap.clear
+    backend = Backends.find(backend_id)
+    if backend
+      @frontend = backend.frontend
+      job = DeployFrontendJobspec.new(@frontend.deploy_frontend_job.id, @frontend.host, "restart_remote_backend", backend.id, backend.name)
+      Delayed::Job.enqueue(job, :queue => "deploy-web")
+      Delayed::Job.enqueue(self, :queue => queue, :run_at => (Time.now + period.seconds))
+    else
+      puts "Backend not found"
+    end
+  end
+end
