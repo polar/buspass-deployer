fb69f61b3c97cf685c574ad5744562d1e12c002d
Polar Humenn <polar@adiron.com>
2013-10-30 17:52:05 -0400

    Unix Endpoint Types

--- app/models/keepalive_restart_backend_job.rb
+++ app/models/keepalive_restart_backend_job.rb
@@ -8,7 +8,7 @@ class KeepaliveRestartBackendJob < Struct.new(:queue, :backend_id, :period)
       hostname = backend.hostname
       host_url = "http://#{hostname}"
       req = open(host_url) do |resp|
-        if resp.status resp.status[0].to_i >= 500
+        if resp.status && resp.status[0].to_i >= 500
           frontend = backend.frontend
           job = DeployFrontendJobspec.new(frontend.deploy_frontend_job.id, frontend.host, "restart_remote_backend", backend.id, backend.name)
           DelayedJob.enqueue(job, :queue => "deploy-web")
