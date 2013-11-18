e0dc9a175eaf2d7eff66fa5f662fbcdaafd6b33a
Polar Humenn <polar@adiron.com>
2013-11-18 16:51:58 -0500

    Unix Endpoint Types

--- app/models/deploy_remote_key_operations.rb
+++ app/models/deploy_remote_key_operations.rb
@@ -5,8 +5,8 @@ module DeployRemoteKeyOperations
   def ssh_cert
     key = RemoteKey.where(:name => installation.ssh_key_name).first
     if key
-      if ! File.exists?(key.ssh_key.file.path) && key.key_encrypted_content(:key => ENV["AWS_SECRET_ACCESS_KEY"])
-        key.decrypt_key_content_to_file
+      if ! File.exists?(key.ssh_key.file.path) && key.key_encrypted_content
+        key.decrypt_key_content_to_file(:key => ENV["AWS_SECRET_ACCESS_KEY"])
       end
       return key.ssh_key.file.path
     end
