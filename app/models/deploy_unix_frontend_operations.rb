055cf33eadcbf4238822673e91f170a19b6038b3
Polar Humenn <polar@adiron.com>
2013-11-19 10:35:26 -0500

    confused

--- app/models/deploy_unix_frontend_operations.rb
+++ app/models/deploy_unix_frontend_operations.rb
@@ -78,9 +78,10 @@ module DeployUnixFrontendOperations
       when /unix/
         uadmin_unix_ssh("sudo adduser #{remote_user} --quiet --disabled-password || exit 0")
       when /ec2/
-        uadmin_unix_ssh("sudo adduser #{remote_user} --create-home --user-group || exit 0")
+        uadmin_unix_ssh("sudo adduser #{remote_user} --create-home --user-group --groups rvm || exit 0")
     end
     uadmin_unix_ssh("sudo -u #{remote_user} mkdir -p ~#{remote_user}/.ssh")
+    uadmin_unix_ssh("sudo -u #{remote_user} chmod 755 ~#{remote_user}")
     uadmin_unix_ssh("sudo -u #{remote_user} chmod 777 ~#{remote_user}/.ssh")
     file = pub_cert(ssh_cert)
     begin
