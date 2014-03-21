6edcf4d7863552f3e69b97b89de9888410883a84
Polar Humenn <polar@adiron.com>
2014-03-21 19:16:38 -0400

    Encryption Fix

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BuspassDeployer::Application.config.secret_token = 'ecdb8447f05ff2eea58babf2699518e090f157b422ebf941bb94e55917da983f855110a8d603fda3eab3de10675265874d7414ec5def618d3a65950c92979cb3'
+BuspassDeployer::Application.config.secret_token = ENV["SESSION_SECRET_KEY"]
